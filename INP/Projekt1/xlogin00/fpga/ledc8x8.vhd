efc7fbb6e809500c3a60c1ee51541db952659cbd
thanhdolong <thanh.dolong@gmail.com>
2019-01-21 18:23:43 +0100

    INP extract folders

---
+++ INP/Projekt1/xlogin00/fpga/ledc8x8.vhd
@@ -0,0 +1,30 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
+
+entity ledc8x8 is
+port ( -- Sem doplnte popis rozhrani obvodu.
+
+);
+end ledc8x8;
+
+architecture main of ledc8x8 is
+
+    -- Sem doplnte definice vnitrnich signalu.
+
+begin
+
+    -- Sem doplnte popis obvodu. Doporuceni: pouzivejte zakladni obvodove prvky
+    -- (multiplexory, registry, dekodery,...), jejich funkce popisujte pomoci
+    -- procesu VHDL a propojeni techto prvku, tj. komunikaci mezi procesy,
+    -- realizujte pomoci vnitrnich signalu deklarovanych vyse.
+
+    -- DODRZUJTE ZASADY PSANI SYNTETIZOVATELNEHO VHDL KODU OBVODOVYCH PRVKU,
+    -- JEZ JSOU PROBIRANY ZEJMENA NA UVODNICH CVICENI INP A SHRNUTY NA WEBU:
+    -- http://merlin.fit.vutbr.cz/FITkit/docs/navody/synth_templates.html.
+
+    -- Nezapomente take doplnit mapovani signalu rozhrani na piny FPGA
+    -- v souboru ledc8x8.ucf.
+
+end main;
